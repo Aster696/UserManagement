d51cb92154c7a9f041eac7883e62990168a6cdff
Aster07102000 <aster.r@codezen.in>
2024-10-16 10:14:38 +0530

    fsit commit

---
+++ authentication/signupGUI.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import messagebox
+from validation.validation import Validation
+from csv_file.csv_file import CSVFile
+
+class SignupGUI:
+    def __init__(self, master, geometry):
+        self.master = master
+        self.master.title('Singup')
+        self.master.geometry(geometry)
+        self.create_widgets()
+
+    def create_widgets(self):
+        tk.Label(self.master, text='Signup', font=('Arial', 16)).pack(pady=10)
+        
+        tk.Label(self.master, text='Name').pack(pady=5)
+        self.name = tk.Entry(self.master, width=50)
+        self.name.pack(pady=5)
+
+        tk.Label(self.master, text='Email').pack(pady=5)
+        self.email = tk.Entry(self.master, width=50)
+        self.email.pack(pady=5)
+
+        tk.Label(self.master, text='Password').pack(pady=5)
+        self.password = tk.Entry(self.master, width=50)
+        self.password.pack(pady=5)
+
+        tk.Label(self.master, text='Confirm Password').pack(pady=5)
+        self.confirm_password = tk.Entry(self.master, width=50)
+        self.confirm_password.pack(pady=5)
+
+        tk.Button(self.master, text='Submit', width=50, command=lambda: self.onSubmit()).pack(pady=20)
+
+    def onSubmit(self):
+
+        if not self.name.get().strip() or not self.email.get().strip() or not self.password.get().strip() or not self.confirm_password.get().strip():
+            messagebox.showinfo('Details required', 'Please fill in all the details')
+            return 
+
+        if Validation.email(self, self.email.get()):
+            messagebox.showinfo('Email', 'Please enter a valid email')
+            return
+
+        if self.password.get() == self.confirm_password.get():
+            messagebox.showinfo('Password', 'Password and Confirm Password do not match')
+            return
+
+        header = ['id', 'name', 'email', 'password']
+        data = [{'name': self.name.get(), 'email': self.email.get(), 'password': self.password.get()}]
+
+        if not CSVFile.save_data(self, 'Users.csv', header, data):
+            messagebox.showerror('Error', 'Email already exist, please try with different email')
+        else:
+            messagebox.showinfo('Success', 'Signup successful!')
+            self.master.destroy()
+
+
--- mainGUI.py
+++ mainGUI.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
+from authentication.signup import Signup
+from authentication.signupGUI import SignupGUI
 
 class UserManagement:
     def __init__(self, master):
@@ -12,17 +14,24 @@ class UserManagement:
         window_width = int(screen_width * 0.5)
         window_height = int(screen_height * 0.5)
 
-        master.geometry(f"{window_width}x{window_height}+{(screen_width-window_width) // 2}+{(screen_height - window_height) // 2}")
+        self.geometry = f"{window_width}x{window_height}+{(screen_width-window_width) // 2}+{(screen_height - window_height) // 2}"
+        master.geometry(self.geometry)
 
         self.create_widgets(window_width)
 
     def create_widgets(self, window_width):
         tk.Label(self.master, text='User Management', font=('Arial', 16)).pack(pady=10)
         # show options and set button width
-        tk.Button(self.master, text='Signup', width=50, command='').pack(pady=5, padx=10)
+        tk.Button(self.master, text='Signup', width=50, command=lambda: self.show_signup()).pack(pady=5, padx=10)
         tk.Button(self.master, text='Login', width=50, command='').pack(pady=5, padx=10)
         tk.Button(self.master, text='Display Users', width=50, command='').pack(pady=5, padx=10)
         tk.Button(self.master, text='Exit', width=50, command=self.master.quit).pack(pady=5, padx=10)
+    
+    def show_signup(self):
+        for widget in self.master.winfo_children():
+            widget.destroy()
+        SignupGUI(self.master, self.geometry)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
