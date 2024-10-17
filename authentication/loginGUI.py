12d0200d4228903ff505a6bd0a75df4177dbb36d
Aster07102000 <aster.r@codezen.in>
2024-10-17 10:22:18 +0530

    fsit commit

---
+++ authentication/loginGUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class LoginGUI:
+    
+    def __init__(self, master, geometry):
+        pass
+
+    def create_widgets(self):
+        pass
